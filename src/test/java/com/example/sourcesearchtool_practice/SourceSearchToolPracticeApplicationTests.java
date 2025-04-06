7600e275fdb2bce524c99cdf30b2884d085d39c2
subCharacters <wldns59@naver.com>
2025-04-06 18:44:36 +0900

    initial commit

---
+++ src/test/java/com/example/sourcesearchtool_practice/SourceSearchToolPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sourcesearchtool_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceSearchToolPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
