e3c0136c281be2a15e2216bf5cdac0d432f97551
subCharacters <wldns@naver.com>
2025-04-09 22:32:34 +0900

    ãadd top page testing.

--- src/main/java/com/example/sourcesearchtool_practice/contorller/TopController.java
+++ src/main/java/com/example/sourcesearchtool_practice/contorller/TopController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloWorld {
-    @GetMapping("/helloworld")
-    public String helloWorld() {
-        return "HelloWorld";
+public class TopController {
+    @GetMapping("/top")
+    public String top() {
+        return "top";
     }
 }
